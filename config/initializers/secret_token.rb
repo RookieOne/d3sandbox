d67eef2e1a745f3b7c7bfc454a7178a344b3dd6c
Jonathan Birkholz <rookieone@gmail.com>
2013-07-24 09:55:27 -0500

    rails install

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+D3sandbox::Application.config.secret_key_base = 'ca744e037afb88ce268c66150c43286272f198d2bf1a91d4b7c0b12515cf775ae057bde0c814f8a959573f3348294b99629f5ab7e5b1f6cfd9e57c7d0f7dc69e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+D3sandbox::Application.config.session_store :cookie_store, key: '_d3sandbox_session'
